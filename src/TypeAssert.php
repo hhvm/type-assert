5881d1c2bfab0f4c10545c43aec64408b5977a83
Fred Emmott <fred@fredemmott.co.uk>
2016-09-27 19:28:32 -0700

    Validate traversable and keyedtraversables in type structures

--- src/TypeAssert.php
+++ src/TypeAssert.php
@@ -9,7 +9,7 @@
 
 namespace FredEmmott\TypeAssert;
 
-use \TypeStructureKind as Kind;
+use FredEmmott\TypeAssert\Private\TypeStructureImpl;
 
 abstract class TypeAssert {
   final public static function isString(mixed $x): string {
@@ -99,51 +99,7 @@ abstract class TypeAssert {
     TypeStructure<T> $ts,
     mixed $value,
   ): T {
-    switch ($ts['kind']) {
-      case Kind::OF_VOID:
-        throw new UnsupportedTypeException('OF_VOID');
-      case Kind::OF_INT:
-        return /* HH_IGNORE_ERROR[4110] */ self::isInt($value);
-      case Kind::OF_BOOL:
-        return /* HH_IGNORE_ERROR[4110] */ self::isBool($value);
-      case Kind::OF_FLOAT:
-        return /* HH_IGNORE_ERROR[4110] */ self::isFloat($value);
-      case Kind::OF_STRING:
-        return /* HH_IGNORE_ERROR[4110] */ self::isString($value);
-      case Kind::OF_RESOURCE:
-        return /* HH_IGNORE_ERROR[4110] */ self::isResource($value);
-      case Kind::OF_NUM:
-        return /* HH_IGNORE_ERROR[4110] */ self::isNum($value);
-      case Kind::OF_ARRAYKEY:
-        return /* HH_IGNORE_ERROR[4110] */ self::isArrayKey($value);
-      case Kind::OF_NORETURN:
-        throw new UnsupportedTypeException('OF_NORETURN');
-      case Kind::OF_MIXED:
-        return /* HH_IGNORE_ERROR[4110] */ $value;
-      case Kind::OF_TUPLE:
-        // FIXME
-      case Kind::OF_FUNCTION:
-        throw new UnsupportedTypeException('OF_FUNCTION');
-      case Kind::OF_ARRAY:
-        // FIXME
-      case Kind::OF_GENERIC:
-        throw new UnsupportedTypeException('OF_GENERIC');
-      case Kind::OF_SHAPE:
-        // FIXME
-      case Kind::OF_CLASS:
-      case Kind::OF_INTERFACE:
-        // FIXME: validate classes
-        $class = self::isNotNull($ts['classname']);
-        return /* HH_IGNORE_ERROR[4110] */ self::isInstanceOf(
-          self::isNotNull($ts['classname']),
-          $value,
-        );
-      case Kind::OF_TRAIT:
-        throw new UnsupportedTypeException('OF_UNRESOLVED');
-      case Kind::OF_ENUM:
-        // FIXME
-      case Kind::OF_UNRESOLVED:
-        throw new UnsupportedTypeException('OF_UNRESOLVED');
-    }
+    TypeStructureImpl::assertMatchesTypeStructure($ts, $value);
+    return /* HH_IGNORE_ERROR[4110] */ $value;
   }
 }
---
+++ src/private/TypeStructureImpl.php
@@ -0,0 +1,121 @@
+<?hh // strict
+/*
+ *  Copyright (c) 2016, Fred Emmott
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the ISC license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+
+namespace FredEmmott\TypeAssert\Private;
+
+use \FredEmmott\TypeAssert\TypeAssert;
+use \FredEmmott\TypeAssert\UnsupportedTypeException;
+use \TypeStructureKind as Kind;
+
+abstract final class TypeStructureImpl {
+  final public static function assertMatchesTypeStructure<T>(
+    TypeStructure<T> $ts,
+    mixed $value,
+  ): void {
+    switch ($ts['kind']) {
+      case Kind::OF_VOID:
+        throw new UnsupportedTypeException('OF_VOID');
+      case Kind::OF_INT:
+        TypeAssert::isInt($value);
+        return;
+      case Kind::OF_BOOL:
+        TypeAssert::isBool($value);
+        return;
+      case Kind::OF_FLOAT:
+        TypeAssert::isFloat($value);
+        return;
+      case Kind::OF_STRING:
+        TypeAssert::isString($value);
+        return;
+      case Kind::OF_RESOURCE:
+        TypeAssert::isResource($value);
+        return;
+      case Kind::OF_NUM:
+        TypeAssert::isNum($value);
+        return;
+      case Kind::OF_ARRAYKEY:
+        TypeAssert::isArrayKey($value);
+        return;
+      case Kind::OF_NORETURN:
+        throw new UnsupportedTypeException('OF_NORETURN');
+      case Kind::OF_MIXED:
+        return;
+      case Kind::OF_TUPLE:
+        // FIXME
+      case Kind::OF_FUNCTION:
+        throw new UnsupportedTypeException('OF_FUNCTION');
+      case Kind::OF_ARRAY:
+        // FIXME
+      case Kind::OF_GENERIC:
+        throw new UnsupportedTypeException('OF_GENERIC');
+      case Kind::OF_SHAPE:
+        // FIXME
+      case Kind::OF_CLASS:
+      case Kind::OF_INTERFACE:
+        $class = TypeAssert::isNotNull($ts['classname']);
+
+        if (is_a($class, KeyedTraversable::class, /* strings = */ true)) {
+          list($keys_ts, $values_ts) = TypeAssert::isNotNull(
+            $ts['generic_types'],
+          );
+          self::assertKeyAndValueTypes(
+            $keys_ts,
+            $values_ts,
+            /* HH_IGNORE_ERROR[4110] */ $value,
+          );
+        } else if (
+          is_a($class, Traversable::class, /* strings = */ true)
+        ) {
+          list($values_ts) = TypeAssert::isNotNull(
+            $ts['generic_types'],
+          );
+          self::assertValueTypes(
+            $values_ts,
+            /* HH_IGNORE_ERROR[4110] */ $value,
+          );
+        }
+
+        TypeAssert::isInstanceOf(
+          TypeAssert::isNotNull($ts['classname']),
+          $value,
+        );
+        return;
+      case Kind::OF_TRAIT:
+        throw new UnsupportedTypeException('OF_UNRESOLVED');
+      case Kind::OF_ENUM:
+        // FIXME
+      case Kind::OF_UNRESOLVED:
+        throw new UnsupportedTypeException('OF_UNRESOLVED');
+    }
+    invariant_violation(
+      'Unsupported kind: %s',
+      Kind::getNames()[$ts['kind']] ?? var_export($ts['kind'], true),
+    );
+  }
+
+  public static function assertKeyAndValueTypes<Tk, Tv>(
+    TypeStructure<Tk> $key_ts,
+    TypeStructure<Tv> $value_ts,
+    KeyedTraversable<mixed, mixed> $traversable,
+  ): void {
+    foreach ($traversable as $key => $value) {
+      self::assertMatchesTypeStructure($key_ts, $key);
+      self::assertMatchesTypeStructure($value_ts, $value);
+    }
+  }
+
+  public static function assertValueTypes<Tk, Tv>(
+    TypeStructure<Tv> $value_ts,
+    KeyedTraversable<mixed, mixed> $traversable,
+  ): void {
+    foreach ($traversable as $value) {
+      self::assertMatchesTypeStructure($value_ts, $value);
+    }
+  }
+}
