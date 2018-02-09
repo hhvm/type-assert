1a8e120158d5018bda505eb6116b4fdcb10382c2
Fred Emmott <fe@fb.com>
2018-02-09 09:02:30 -0800

    Don't use Docker on Travis

--- .travis.sh
+++ .travis.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 set -ex
 hhvm --version
-echo hhvm.jit=0 >> /etc/hhvm/php.ini
-curl https://getcomposer.org/installer | hhvm --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
 
-cd /var/source
-hhvm /usr/local/bin/composer install
-hh_server --check $(pwd)
-hhvm -d hhvm.php7.all=0 vendor/bin/phpunit tests/
-hhvm -d hhvm.php7.all=1 vendor/bin/phpunit tests/
+composer install
 
-sed -i '/enable_experimental/d' .hhconfig
+hh_client
+
+hhvm vendor/bin/phpunit tests/
+
+echo > .hhconfig
 hh_server --check $(pwd)
