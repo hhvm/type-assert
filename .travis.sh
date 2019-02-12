602e88c1a024caa683e5a5c8648019499fbd6430
Fred Emmott <fe@fb.com>
2019-02-12 15:38:40 -0800

    Test using Docker

--- .travis.sh
+++ .travis.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 set -ex
+apt update -y
+DEBIAN_FRONTEND=noninteractive apt install -y php-cli zip unzip
 hhvm --version
-echo $TRAVIS_PHP_VERSION
+php --version
 
-if [ "$TRAVIS_PHP_VERSION" = 'hhvm-3.24' ]; then
-  cp composer.lock-3.24 composer.lock
-fi
-
+(
+  cd $(mktemp -d)
+  curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+)
 composer install
 
 hh_client
-
-hhvm vendor/bin/hacktest tests/*.php
-
-echo > .hhconfig
-hh_server --check $(pwd)
+hhvm vendor/bin/hacktest tests/
+if !(hhvm --version | grep -q -- -dev); then
+  hhvm vendor/bin/hhast-lint
+fi
